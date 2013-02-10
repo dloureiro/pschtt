3bc9a08de7ccfe2ce10095c1f8f9a0236ad63386
David Loureiro <david.loureiro@sysfera.com>
2013-02-10 21:22:44 +0100

    Reusing the form to get new widget info

--- public/js/widgets/new.js
+++ public/js/widgets/new.js
@@ -1,15 +1,12 @@
 $(document).ready(function() {
-    $('#submit').click(function(event) {
-        var name = $("#widgetname").val();
-        var price = $("#widgetprice").val();
-	//        alert("Adding widget with name : " + name + " and price " + price);
-        var jqXHR = $.post("/widgets/create", {widgetname:name, widgetprice:price},
-        function(data,status) {
-            //alert(data + " : " + status);
+    $('#myForm').submit(function(event) {
+        event.preventDefault();
+        var inputData = $('#myForm').serialize();
+        var url = $('#myForm').attr( 'action' );
+        $.post(url, inputData,
+        function(data, status) {
+            alert(data + " : " + status);
             window.location.href = "/widgets/" + data.widget.id + "/show";
         }, 'json');
-	    //.done(function() { alert("second success"); })
-	    //.fail(function() { alert("error"); })
-	    //.always(function() { alert("finished"); });
     });
 });
