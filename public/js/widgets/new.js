67c5e8fb9cda2376712e683418320d2cb7acf8e9
David Loureiro <david.loureiro@sysfera.com>
2013-02-10 20:00:12 +0100

    adding the redirection to the show page for the newly created widget

--- public/js/widgets/new.js
+++ public/js/widgets/new.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
     $('#submit').click(function(event) {
         var jqXHR = $.post("/widgets/create", {widgetname:"bla", widgetprice:10},
         function(data,status) {
-            alert(data + " : " + status);
-            //window.location.href = "/widgets/" + data.id;
-        }, 'json')
-        .done(function() { alert("second success"); })
-        .fail(function() { alert("error"); })
-        .always(function() { alert("finished"); });
+            //alert(data + " : " + status);
+            window.location.href = "/widgets/" + data.widget.id;
+        }, 'json');
+	    //.done(function() { alert("second success"); })
+	    //.fail(function() { alert("error"); })
+	    //.always(function() { alert("finished"); });
     });
 });
