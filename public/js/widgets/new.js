c6c5a25fe8023d8f0b775b3624fd390b9a1910cb
augustin <augustin@augustin-sysfera.(none)>
2013-02-10 20:40:16 +0100

    Fiddling a bit with widget creation

--- public/js/widgets/new.js
+++ public/js/widgets/new.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
     $('#submit').click(function(event) {
-        var jqXHR = $.post("/widgets/create", {widgetname:"bla", widgetprice:10},
+	alert($('#myForm').serialize());
+        var jqXHR = $.post("/widgets/create", $('#myForm').serialize(),
         function(data,status) {
             alert(data + " : " + status);
-            //window.location.href = "/widgets/" + data.id;
+            window.location.href = "/widgets/" + data.widget.id;
         }, 'json')
         .done(function() { alert("second success"); })
         .fail(function() { alert("error"); })
         .always(function() { alert("finished"); });
+	event.preventDefault();
     });
 });
